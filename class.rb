51b89a6e4eb267bb6bcdd828c2c0b36d206c90f1
Hammed Adisa <91697183+AbuRayhaan@users.noreply.github.com>
2022-07-15 11:03:13 +0100

    Modify the class.rb to fix linters error

--- class.rb
+++ class.rb
@@ -3,13 +3,13 @@ require_relative 'module'
 class MyList
   include MyEnumerable
 
-  def initialize (*list)
+  def initialize(*list)
     @list = list
   end
 
   # rubocop:disable Style/ExplicitBlockArgument
   def each
-    @list.each = { |value| yield value }
+    @list.each { |item| yield item }
   end
   # rubocop:enable Style/ExplicitBlockArgument
 end
