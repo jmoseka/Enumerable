e0029cb7efb4510c36b0b8ffbb1f762df2ec19b5
Hammed Adisa <91697183+AbuRayhaan@users.noreply.github.com>
2022-07-15 10:53:09 +0100

    Modified the class.rb to fix linters error

--- class.rb
+++ class.rb
@@ -7,7 +7,9 @@ class MyList
     @list = list
   end
 
+  # rubocop:disable Style/ExplicitBlockArgument
   def each
     @list.each = {|n| yield n}
   end
+  # rubocop:enable Style/ExplicitBlockArgument
 end
