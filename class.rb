1a99cfde0b9e2c4df02d6e16303eae03e5bf9a4c
Brian Nyachae <techconsultants.bree@gmail.com>
2022-07-15 19:05:33 +0300

    add new class myList

--- class.rb
+++ class.rb
@@ -13,3 +13,12 @@ class MyList
   end
   # rubocop:enable Style/ExplicitBlockArgument
 end
+list = MyList.new([1, 2, 3, 4])
+# Test #all?
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
+# Test #any?
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
+# Test #filter?
+print(list.filter?(&:even?))
